79b630ef0ce60a7e5503124b1ce7da348cf7545f
Rami Dahman <dahmanrb@vcu.edu>
2018-04-03 21:43:01 -0400

    afgvysdjkyfhsmf

--- app/src/main/java/com/contigo2/cmsc355/breadboard/QuizInformation.java
+++ app/src/main/java/com/contigo2/cmsc355/breadboard/QuizInformation.java
@@ -1,8 +1,10 @@
 package com.contigo2.cmsc355.breadboard;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class QuizInformation extends AppCompatActivity {
-    private String quizCode, name = "", time = "", numQ = "", qClass = "", dueDate = "";
+    private String quizCode = "", name = "", time = "", numQ = "", qClass = "", dueDate = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +30,39 @@ public class QuizInformation extends AppCompatActivity {
         FirebaseUser user = mAuth.getCurrentUser();
         DatabaseReference getQuizInfo = database.getReference("quiz/" + quizCode);
 
+        final TextView TVname  = findViewById(R.id.QuizTitleField);
+        final TextView TVdueD  = findViewById(R.id.quizDueDateField);
+        final TextView TVnumQ  = findViewById(R.id.quizNumQuestionsField);
+        final TextView TVclass = findViewById(R.id.quizClassField);
+        final TextView TVtime  = findViewById(R.id.quizTimeLimitField);
+
+        final Resources res = getResources();
+
         ValueEventListener listenQuizInfo = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot codeSnapshot: dataSnapshot.getChildren()) {
-                    if(codeSnapshot.getKey().equals("code")) continue;
-                    if(codeSnapshot.getKey().equals("due date")) dueDate = R.string.dueDate + codeSnapshot.getValue(String.class);
-                    if(codeSnapshot.getKey().equals("name")) name = R.string.quizName + codeSnapshot.getValue(String.class);
-                    if(codeSnapshot.getKey().equals("num questions")) numQ = R.string.numQuestions + codeSnapshot.getValue(String.class);
+                    if(codeSnapshot.getKey().equals("code"));
+                    if(codeSnapshot.getKey().equals("due date")) {
+                        dueDate = codeSnapshot.getValue(String.class);
+                        TVdueD.setText(res.getString(R.string.dueDate, dueDate));
+                    }
+                    if(codeSnapshot.getKey().equals("name")) {
+                        name = codeSnapshot.getValue(String.class);
+                        TVname.setText(res.getString(R.string.quizName, name));
+                    }
+                    if(codeSnapshot.getKey().equals("num questions")) {
+                        numQ = codeSnapshot.getValue(String.class);
+                        TVnumQ.setText(res.getString(R.string.numQuestions, numQ));
+                    }
+                    if(codeSnapshot.getKey().equals("class name")) {
+                        qClass = codeSnapshot.getValue(String.class);
+                        TVclass.setText(res.getString(R.string.className, "whoops"));
+                    }
+                    if(codeSnapshot.getKey().equals("time limit")) {
+                        time = codeSnapshot.getValue(String.class);
+                        TVtime.setText(res.getString(R.string.timeLimit, time));
+                    }
                 }
             }
             @Override
@@ -45,15 +72,19 @@ public class QuizInformation extends AppCompatActivity {
         };
         getQuizInfo.addListenerForSingleValueEvent(listenQuizInfo);
 
-        TextView TVname  = findViewById(R.id.QuizTitleField);
-        TextView TVdueD  = findViewById(R.id.quizDueDateField);
-        TextView TVnumQ  = findViewById(R.id.quizNumQuestionsField);
-        TextView TVclass = findViewById(R.id.quizClassField);
+    }
 
-        TVname.setText(name);
-        TVdueD.setText(dueDate);
-        TVnumQ.setText(numQ);
-        TVclass.setText(qClass);
+    public void onButtonClick(View v) {
+        if(v.getId() == R.id.statsBTN) {
+            Intent i = new Intent(QuizInformation.this, quizStatistics.class);
+            startActivity(i);
+        }
+        if(v.getId() == R.id.gradesBTN) {
+            Intent i = new Intent(QuizInformation.this, quizStatistics.class);
+            startActivity(i);
+        }
+        if(v.getId() == R.id.modifyQuizBTN) {
 
+        }
     }
 }
